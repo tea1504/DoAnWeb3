84cb6480409d7b447b6ededff31a0afb093bbaeb
leokaa <linhduy8a5@gmail.com>
2021-01-16 17:56:04 +0700

    migra: (qhgd+lsbt)

---
+++ database/migrations/2021_01_16_103541_create_quanhegiadinh_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuanhegiadinhTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quanhegiadinh', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->unsignedInteger('qhgd_ma')
+                    ->autoIncrement();
+            $table->string('nv_ma', 10);
+            $table->string('qhgd_ten', 50);
+            $table->string('qhgd_moiQuanhe', 50);
+            $table->string('qhgd_namSinh', 4);
+            $table->string('qhgd_diaChi', 100);
+            $table->string('qhgd_ngheNghiep', 100);
+            $table->timestamp('qhgd_taoMoi')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('qhgd_capNhat')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->unsignedInteger('qhgd_nuocNgoai');
+            $table->foreign('nv_ma')
+                    ->references('nv_ma')
+                    ->on('nhanvien')
+                    ->onDelete('CASCADE')
+                    ->onUpdate('CASCADE');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quanhegiadinh');
+    }
+}
---
+++ database/migrations/2021_01_16_104523_create_lichsubanthan_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLichsubanthanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lichsubanthan', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->unsignedInteger('lsbt_ma')
+                    ->autoIncrement();
+            $table->string('nv_ma', 10);
+            $table->text('lsbt_hanhViPhamToi');
+            $table->text('lsbt_thamGiaToChucChinhTri');
+           
+            
+            $table->foreign('nv_ma')
+                    ->references('nv_ma')
+                    ->on('nhanvien')
+                    ->onDelete('CASCADE')
+                    ->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lichsubanthan');
+    }
+}
