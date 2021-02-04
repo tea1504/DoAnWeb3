dfae8d0591ffbcb7b6fae84bd8d6840935610497
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-02-04 21:06:08 +0700

    xong các chức năng in và xuất file

--- resources/views/admin/nhanvien/excel.blade.php
+++ resources/views/admin/nhanvien/excel.blade.php
@@ -19,54 +19,53 @@
 <body>
     <table>
         <tr>
-            <td align="center">
-                
+            <td align="center" colspan="8">
+
             </td>
         </tr>
         <tr>
-            <td align="center">
+            <td align="center" colspan="8">
                 Shin Human Resource Management
             </td>
         </tr>
         <tr>
-            <td align="center">
+            <td align="center" colspan="8">
                 <span class="slogan">Hệ thống quản lý nhân sự</span>
             </td>
         </tr>
         <tr>
-            <td align="center">
+            <td align="center" colspan="8">
                 <span class="slogan">Website: <i><b>http://qlnhanluc.local</b></i></span>
             </td>
         </tr>
-    </table>
-    <table border="1" cellspacing="0" cellpadding="3">
-        <thead>
-            <tr>
-                <th width="5">#</th>
-                <th width="15">Mã cán bộ</th>
-                <th width="20">Ảnh</th>
-                <th width="30">Tên cán bộ</th>
-                <th width="25">Chức vụ</th>
-                <th width="70">Địa chỉ</th>
-                <th width="20">Điện thoại</th>
-                <th width="30">Email</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($dsnv as $nv)
-            <tr>
-                <td align="center"><b>{{$loop->index + 1}}</b></td>
-                <td align="center">{{$nv->nv_ma}}</td>
-                <td align="center">
-                </td>
-                <td>{{$nv->nv_hoTen}}</td>
-                <td>{{$nv->chucVu->cvu_ten}}</td>
-                <td>{{$nv->nv_noiOHienNay}}</td>
-                <td align="center">{{$nv->nv_sdt}}</td>
-                <td>{{$nv->nv_email}}</td>
-            </tr>
-            @endforeach
-        </tbody>
+        <tr>
+            <td align="center" colspan="8">
+
+            </td>
+        </tr>
+        <tr>
+            <th width="5">#</th>
+            <th width="15">Mã cán bộ</th>
+            <th width="20">Ảnh</th>
+            <th width="30">Tên cán bộ</th>
+            <th width="25">Chức vụ</th>
+            <th width="70">Địa chỉ</th>
+            <th width="20">Điện thoại</th>
+            <th width="30">Email</th>
+        </tr>
+        @foreach($dsnv as $nv)
+        <tr>
+            <td align="center"><b>{{$loop->index + 1}}</b></td>
+            <td align="center">{{$nv->nv_ma}}</td>
+            <td align="center">
+            </td>
+            <td>{{$nv->nv_hoTen}}</td>
+            <td>{{$nv->chucVu->cvu_ten}}</td>
+            <td>{{$nv->nv_noiOHienNay}}</td>
+            <td align="center">{{$nv->nv_sdt}}</td>
+            <td>{{$nv->nv_email}}</td>
+        </tr>
+        @endforeach
     </table>
 </body>
 
