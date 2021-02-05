f98240ee0167ca08e4c6fca429e724ff6e0ae8c4
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-02-05 22:18:52 +0700

    xong chức năng đăng nhập

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -11,126 +11,141 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('themes/AdminLTE/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                <?php
+                if (Session::has('user')) :
+                    $user = Session::get('user')[0];;
+                ?>
+                    <img src="{{ Storage::exists('public/avatar/' . $user->nv_anh) ? asset('storage/avatar/' . $user->nv_anh) : asset('storage/avatar/default.png') }}" class="img-circle elevation-2 bg-white" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">{{$user->nv_hoTen}}</a>
             </div>
+        <?php
+                else :
+        ?>
+            <img src="#" class="img-circle elevation-2 bg-white" alt="User">
         </div>
+        <div class="info">
+            <a href="#" class="d-block"></a>
+        </div>
+    <?php
+                endif;
+    ?>
+    </div>
 
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-                <li class="nav-item has-treeview menu-open">
-                    <a href="{{ route('admin') }}" class="{{ request()->is('admin')?'active nav-link':'nav-link' }}">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Dashboard
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="{{ request()->is('admin/nhanvien')?'active nav-link':'nav-link' }}">
-                        <i class="nav-icon fas fa-users"></i>
-                        <p>
-                            Nhân viên
+            <li class="nav-item has-treeview menu-open">
+                <a href="{{ route('admin') }}" class="{{ request()->is('admin')?'active nav-link':'nav-link' }}">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Dashboard
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item has-treeview">
+                <a href="#" class="{{ request()->is('admin/nhanvien')?'active nav-link':'nav-link' }}">
+                    <i class="nav-icon fas fa-users"></i>
+                    <p>
+                        Nhân viên
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{ route('admin.nhanvien.index') }}" class="nav-link">
+                            <i class="fas fa-user nav-icon"></i>
+                            <p>Danh sách</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/charts/flot.html" class="nav-link">
+                            <i class="fas fa-user-plus nav-icon"></i>
+                            <p>Thêm mới</p>
+                        </a>
+                    </li>
+                </ul>
+
+            </li>
+            <li class="nav-item has-treeview">
+                <a href="#" class="{{ (request()->is('admin/khenthuong')||request()->is('admin/kyluat'))?'active nav-link':'nav-link' }}">
+                    <i class="nav-icon fas fa-medal"></i>
+                    <p>
+                        Khen thưởng/kỷ luật
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{ route('admin.khenthuong.index') }}" class="{{ request()->is('admin/khenthuong')?'active nav-link':'nav-link' }}">
+                            <i class="nav-icon fas fa-check-circle"></i>
+                            <p>Khen thưởng</p>
                             <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('admin.nhanvien.index') }}" class="nav-link">
-                                <i class="fas fa-user nav-icon"></i>
-                                <p>Danh sách</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/charts/flot.html" class="nav-link">
-                                <i class="fas fa-user-plus nav-icon"></i>
-                                <p>Thêm mới</p>
-                            </a>
-                        </li>
-                    </ul>
-
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="{{ (request()->is('admin/khenthuong')||request()->is('admin/kyluat'))?'active nav-link':'nav-link' }}">
-                        <i class="nav-icon fas fa-medal"></i>
-                        <p>
-                            Khen thưởng/kỷ luật
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('admin.khenthuong.index') }}" class="nav-link">
+                                    <i class="nav-icon fas fa-list-alt"></i>
+                                    <p>Danh sách</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages/charts/flot.html" class="nav-link">
+                                    <i class="nav-icon fas fa-plus-circle"></i>
+                                    <p>Thêm mới</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('admin.kyluat.index') }}" class="{{ request()->is('admin/kyluat')?'active nav-link':'nav-link' }}">
+                            <i class="nav-icon fas fa-exclamation-circle"></i>
+                            <p>Kỷ luật</p>
                             <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('admin.khenthuong.index') }}" class="{{ request()->is('admin/khenthuong')?'active nav-link':'nav-link' }}">
-                                <i class="nav-icon fas fa-check-circle"></i>
-                                <p>Khen thưởng</p>
-                                <i class="right fas fa-angle-left"></i>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="{{ route('admin.khenthuong.index') }}" class="nav-link">
-                                        <i class="nav-icon fas fa-list-alt"></i>
-                                        <p>Danh sách</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages/charts/flot.html" class="nav-link">
-                                        <i class="nav-icon fas fa-plus-circle"></i>
-                                        <p>Thêm mới</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('admin.kyluat.index') }}" class="{{ request()->is('admin/kyluat')?'active nav-link':'nav-link' }}">
-                                <i class="nav-icon fas fa-exclamation-circle"></i>
-                                <p>Kỷ luật</p>
-                                <i class="right fas fa-angle-left"></i>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="{{ route('admin.kyluat.index') }}" class="nav-link">
-                                        <i class="nav-icon fas fa-list-alt"></i>
-                                        <p>Danh sách</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages/charts/flot.html" class="nav-link">
-                                        <i class="nav-icon fas fa-plus-circle"></i>
-                                        <p>Thêm mới</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('admin.kyluat.index') }}" class="nav-link">
+                                    <i class="nav-icon fas fa-list-alt"></i>
+                                    <p>Danh sách</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages/charts/flot.html" class="nav-link">
+                                    <i class="nav-icon fas fa-plus-circle"></i>
+                                    <p>Thêm mới</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
 
 
 
 
-                <li class="nav-item has-treeview">
-                    <a href="#" class="{{ request()->is('admin/thongke')?'active nav-link':'nav-link' }}">
-                        <i class="nav-icon fas fa-chart-bar"></i>
-                        <p>
-                            Thống kê
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="{{ route('admin.lienhe') }}" class="{{ request()->is('admin/lienhe')?'active nav-link':'nav-link' }}">
-                        <i class="nav-icon fas fa-envelope"></i>
-                        <p>
-                            Liên hệ
-                        </p>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
+            <li class="nav-item has-treeview">
+                <a href="#" class="{{ request()->is('admin/thongke')?'active nav-link':'nav-link' }}">
+                    <i class="nav-icon fas fa-chart-bar"></i>
+                    <p>
+                        Thống kê
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item has-treeview">
+                <a href="{{ route('admin.lienhe') }}" class="{{ request()->is('admin/lienhe')?'active nav-link':'nav-link' }}">
+                    <i class="nav-icon fas fa-envelope"></i>
+                    <p>
+                        Liên hệ
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
 </aside>
